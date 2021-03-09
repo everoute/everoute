61dbe1aae3fde041aa4a7955306be6990040028a
chengcheng.luo <chengcheng.luo@smartx.com>
2021-03-09 04:30:46 -0500

    Add policyrule apis

--- go.mod
+++ go.mod
@@ -8,13 +8,20 @@ require (
 	github.com/cenkalti/hub v1.0.1 // indirect
 	github.com/cenkalti/rpc2 v0.0.0-20210220005819-4a29bc83afe1 // indirect
 	github.com/contiv/libovsdb v0.0.0-20160406174930-bbc744d8ddc8
+	github.com/go-logr/logr v0.4.0 // indirect
 	github.com/go-openapi/spec v0.19.3
-	github.com/onsi/ginkgo v1.11.0
-	github.com/onsi/gomega v1.8.1
-	k8s.io/api v0.18.4
-	k8s.io/apimachinery v0.18.4
-	k8s.io/client-go v0.18.4
+	github.com/kr/text v0.2.0 // indirect
+	github.com/matttproud/golang_protobuf_extensions v1.0.2-0.20181231171920-c182affec369 // indirect
+	github.com/niemeyer/pretty v0.0.0-20200227124842-a10e7caefd8e // indirect
+	github.com/onsi/ginkgo v1.13.0
+	github.com/onsi/gomega v1.10.1
+	github.com/prometheus/client_golang v1.7.1 // indirect
+	github.com/prometheus/procfs v0.2.0 // indirect
+	gopkg.in/check.v1 v1.0.0-20200227125254-8fa46927fb4f // indirect
+	k8s.io/api v0.20.1
+	k8s.io/apimachinery v0.20.1
+	k8s.io/client-go v0.20.1
 	k8s.io/klog v1.0.0
-	k8s.io/kube-openapi v0.0.0-20200410145947-61e04a5be9a6
+	k8s.io/kube-openapi v0.0.0-20201113171705-d219536bb9fd
 	sigs.k8s.io/controller-runtime v0.6.0
 )
---
+++ pkg/apis/policyrule/v1alpha1/policyrule_types.go
@@ -0,0 +1,85 @@
+/*
+Copyright 2021 The Lynx Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package v1alpha1
+
+import (
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+)
+
+// +genclient
+// +k8s:deepcopy-gen:interfaces=k8s.io/apimachinery/pkg/runtime.Object
+
+// PolicyRule
+// +k8s:openapi-gen=true
+type PolicyRule struct {
+	metav1.TypeMeta   `json:",inline"`
+	metav1.ObjectMeta `json:"metadata,omitempty"`
+
+	Spec   PolicyRuleSpec   `json:"spec,omitempty"`
+	Status PolicyRuleStatus `json:"status,omitempty"`
+}
+
+// PolicyRuleList
+// +k8s:deepcopy-gen:interfaces=k8s.io/apimachinery/pkg/runtime.Object
+type PolicyRuleList struct {
+	metav1.TypeMeta `json:",inline"`
+	metav1.ListMeta `json:"metadata,omitempty"`
+
+	Items []PolicyRule `json:"items"`
+}
+
+// PolicyRuleSpec defines the desired state of PolicyRule
+type PolicyRuleSpec struct {
+	RuleId     string        `json:"ruleId"`
+	Direction  RuleDirection `json:"direction"`
+	Tier       string        `json:"tier,omitempty"`
+	Priority   int32         `json:"priority"`
+	SrcIpAddr  string        `json:"srcIpAddr,omitempty"`
+	DstIpAddr  string        `json:"dstIpAddr,omitempty"`
+	IpProtocol string        `json:"ipProtocol"`
+	SrcPort    uint16        `json:"srcPort,omitempty"`
+	DstPort    uint16        `json:"dstPort,omitempty"`
+	TcpFlags   string        `json:"tcpFlags"`
+	Action     RuleAction    `json:"action"`
+}
+
+type RuleAction string
+
+const (
+	RuleActionAllow RuleAction = "Allow"
+	RuleActionDrop  RuleAction = "Drop"
+)
+
+type RuleDirection string
+
+const (
+	RuleDirectionIn  RuleDirection = "Ingress"
+	RuleDirectionOut RuleDirection = "Egress"
+)
+
+type PolicyRuleEnforceState string
+
+const (
+	PolicyRuleSuccessEnforced PolicyRuleEnforceState = "SuccessEnforced"
+	PolicyRuleFailedEnforced  PolicyRuleEnforceState = "FailedEnforced"
+)
+
+// PolicyRuleStatus defines the observed state of PolicyRule
+type PolicyRuleStatus struct {
+	EnforceState    PolicyRuleEnforceState `json:"enforceState"`
+	MatchStatistics int64                  `json:"matchStatistics"`
+}
