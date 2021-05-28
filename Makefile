zwtop/lynx

e2e framework code refactoring

Makefile
@@ -3,19 +3,23 @@ APISERVER_BOOT=$(shell which apiserver-boot)
 
 all: codegen manifests bin
 
-bin: controller
+bin: controller agent e2e-tools plugins
 
 images:
 	docker build -f build/images/release/Dockerfile -t lynx/release .
 
-controller: fmt vet
+controller:
 	CGO_ENABLED=0 go build -o bin/lynx-controller cmd/lynx-controller/main.go
 
 agent:
 	go build -o bin/lynx-agent cmd/lynx-agent/*.go
 
-e2ectl:
-	CGO_ENABLED=0 go build -o bin/e2ectl tests/e2e/tools/e2ectl.go
+e2e-tools:
+	CGO_ENABLED=0 go build -o bin/e2ectl tests/e2e/tools/e2ectl/*.go
+	CGO_ENABLED=0 go build -o bin/web-utils tests/e2e/tools/web-utils/*.go
+
+plugins:
+	CGO_ENABLED=0 go build -o bin/lynx-tower-plugin plugin/tower/cmd/*.go
 
 test:
 	go test ./pkg/...

tests/e2e/tools/web-utils/command/connect.go
@@ -1,0 +1,150 @@
+/*
+Copyright 2021 The Lynx Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package command
+
+import (
+	"fmt"
+	"io"
+	"net"
+	"strings"
+	"time"
+
+	"github.com/go-ping/ping"
+	"github.com/spf13/cobra"
+)
+
+func NewConnectCommand() *cobra.Command {
+	var server, protocol string
+	var timeout time.Duration
+	var packetNum, passScore int
+
+	cmd := &cobra.Command{
+		Use:   "connect [options]",
+		Short: "Connect test network connection to web-utils server",
+		RunE: func(cmd *cobra.Command, args []string) error {
+			return runConnect(server, protocol, packetNum, passScore, timeout)
+		},
+	}
+
+	cmd.PersistentFlags().IntVarP(&packetNum, "packet-num", "n", 1, "send packet numbers")
+	cmd.PersistentFlags().IntVarP(&passScore, "pass-score", "o", 100, "pass score, must between 0-100")
+	cmd.PersistentFlags().StringVarP(&server, "server", "s", "", "web-utils server address")
+	cmd.PersistentFlags().StringVarP(&protocol, "protocol", "p", "tcp", "connection protocol")
+	cmd.PersistentFlags().DurationVarP(&timeout, "timeout", "t", time.Second, "timeout for connection")
+
+	return cmd
+}
+
+func runConnect(server string, protocol string, packetNum, passScore int, timeout time.Duration) error {
+	var receive int
+	var err error
+
+	switch strings.ToLower(protocol) {
+	case "tcp":
+		receive, err = connectTCP(server, packetNum, timeout)
+	case "udp":
+		receive, err = connectUDP(server, packetNum, timeout)
+	case "icmp":
+		receive, err = connectICMP(server, packetNum, timeout)
+	default:
+		return fmt.Errorf("unsupport protocol %s", protocol)
+	}
+
+	if err != nil {
+		return fmt.Errorf("connect to %s: %s", server, err)
+	}
+
+	if 100*receive/packetNum < passScore {
+		return fmt.Errorf("send %d, receive %d, score too low, %d%% (real) < %d%% (want)", packetNum, receive, 100*receive/packetNum, passScore)
+	}
+	return nil
+}
+
+func connectTCP(server string, num int, timeout time.Duration) (int, error) {
+	conn, err := net.DialTimeout("tcp", server, timeout)
+	if err != nil {
+		return 0, err
+	}
+	defer conn.Close()
+
+	return connectRead(conn, num, timeout)
+}
+
+func connectUDP(server string, num int, timeout time.Duration) (int, error) {
+	conn, err := net.DialTimeout("udp", server, timeout)
+	if err != nil {
+		return 0, err
+	}
+	defer conn.Close()
+
+	return connectRead(conn, num, timeout)
+}
+
+func connectICMP(server string, num int, timeout time.Duration) (int, error) {
+	pinger, err := ping.NewPinger(server)
+	if err != nil {
+		return 0, err
+	}
+
+	pinger.SetPrivileged(true)
+	pinger.Count = num
+	pinger.OnRecv = func(pkt *ping.Packet) {
+		fmt.Printf("%d bytes from %s: icmp_seq=%d time=%v\n", pkt.Nbytes, pkt.IPAddr, pkt.Seq, pkt.Rtt)
+	}
+	if timeout != 0 {
+		pinger.Timeout = timeout
+	}
+	if err = pinger.Run(); err != nil {
+		return 0, err
+	}
+
+	return pinger.Statistics().PacketsRecv, nil
+}
+
+func connectRead(conn net.Conn, num int, timeout time.Duration) (int, error) {
+	if timeout != 0 {
+		err := conn.SetDeadline(time.Now().Add(timeout))
+		if err != nil {
+			return 0, err
+		}
+	}
+
+	var succeed int
+
+	for i := 0; i < num; i++ {
+		if _, err := conn.Write(nil); err != nil && err != io.EOF {
+			fmt.Println(err)
+			continue
+		}
+
+		var buff = [2]byte{}
+		if _, err := conn.Read(buff[:]); err != nil {
+			fmt.Println(err)
+			continue
+		}
+
+		if string(buff[:]) != "ok" {
+			fmt.Println("receive unexpect response: ", string(buff[:]))
+			continue
+		}
+
+		succeed++
+		fmt.Printf("connection sussess to server %s\n", conn.RemoteAddr())
+	}
+
+	return succeed, nil
+}

everoute/everoute

Makefile
@@ -3,19 +3,23 @@ APISERVER_BOOT=$(shell which apiserver-boot)
 
 all: codegen manifests bin
 
-bin: controller
+bin: controller agent e2e-tools plugins
 
 images:
 	docker build -f build/images/release/Dockerfile -t lynx/release .
 
-controller: fmt vet
+controller:
 	CGO_ENABLED=0 go build -o bin/lynx-controller cmd/lynx-controller/main.go
 
 agent:
-	go build -o bin/lynx-agent cmd/lynx-agent/*.go
+	CGO_ENABLED=0 go build -o bin/lynx-agent cmd/lynx-agent/*.go
 
-e2ectl:
-	CGO_ENABLED=0 go build -o bin/e2ectl tests/e2e/tools/e2ectl.go
+e2e-tools:
+	CGO_ENABLED=0 go build -o bin/e2ectl tests/e2e/tools/e2ectl/*.go
+	CGO_ENABLED=0 go build -o bin/net-utils tests/e2e/tools/net-utils/*.go
+
+plugins:
+	CGO_ENABLED=0 go build -o bin/lynx-plugin-tower plugin/tower/cmd/*.go
 
 test:
 	go test ./pkg/...

tests/e2e/tools/net-utils/command/connect.go
@@ -1,0 +1,150 @@
+/*
+Copyright 2021 The Lynx Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package command
+
+import (
+	"fmt"
+	"io"
+	"net"
+	"strings"
+	"time"
+
+	"github.com/go-ping/ping"
+	"github.com/spf13/cobra"
+)
+
+func NewConnectCommand() *cobra.Command {
+	var server, protocol string
+	var timeout time.Duration
+	var packetNum, passScore int
+
+	cmd := &cobra.Command{
+		Use:   "connect [options]",
+		Short: "Connect test network connection to net-utils server",
+		RunE: func(cmd *cobra.Command, args []string) error {
+			return runConnect(server, protocol, packetNum, passScore, timeout)
+		},
+	}
+
+	cmd.PersistentFlags().IntVarP(&packetNum, "packet-num", "n", 1, "send packet numbers")
+	cmd.PersistentFlags().IntVarP(&passScore, "pass-score", "o", 100, "pass score, must between 0-100")
+	cmd.PersistentFlags().StringVarP(&server, "server", "s", "", "net-utils server address")
+	cmd.PersistentFlags().StringVarP(&protocol, "protocol", "p", "tcp", "connection protocol")
+	cmd.PersistentFlags().DurationVarP(&timeout, "timeout", "t", time.Second, "timeout for connection")
+
+	return cmd
+}
+
+func runConnect(server string, protocol string, packetNum, passScore int, timeout time.Duration) error {
+	var receive int
+	var err error
+
+	switch strings.ToLower(protocol) {
+	case "tcp":
+		receive, err = connectTCP(server, packetNum, timeout)
+	case "udp":
+		receive, err = connectUDP(server, packetNum, timeout)
+	case "icmp":
+		receive, err = connectICMP(server, packetNum, timeout)
+	default:
+		return fmt.Errorf("unsupport protocol %s", protocol)
+	}
+
+	if err != nil {
+		return fmt.Errorf("connect to %s: %s", server, err)
+	}
+
+	if 100*receive/packetNum < passScore {
+		return fmt.Errorf("send %d, receive %d, score too low, %d%% (real) < %d%% (want)", packetNum, receive, 100*receive/packetNum, passScore)
+	}
+	return nil
+}
+
+func connectTCP(server string, num int, timeout time.Duration) (int, error) {
+	conn, err := net.DialTimeout("tcp", server, timeout)
+	if err != nil {
+		return 0, err
+	}
+	defer conn.Close()
+
+	return connectRead(conn, num, timeout)
+}
+
+func connectUDP(server string, num int, timeout time.Duration) (int, error) {
+	conn, err := net.DialTimeout("udp", server, timeout)
+	if err != nil {
+		return 0, err
+	}
+	defer conn.Close()
+
+	return connectRead(conn, num, timeout)
+}
+
+func connectICMP(server string, num int, timeout time.Duration) (int, error) {
+	pinger, err := ping.NewPinger(server)
+	if err != nil {
+		return 0, err
+	}
+
+	pinger.SetPrivileged(true)
+	pinger.Count = num
+	pinger.OnRecv = func(pkt *ping.Packet) {
+		fmt.Printf("%d bytes from %s: icmp_seq=%d time=%v\n", pkt.Nbytes, pkt.IPAddr, pkt.Seq, pkt.Rtt)
+	}
+	if timeout != 0 {
+		pinger.Timeout = timeout
+	}
+	if err = pinger.Run(); err != nil {
+		return 0, err
+	}
+
+	return pinger.Statistics().PacketsRecv, nil
+}
+
+func connectRead(conn net.Conn, num int, timeout time.Duration) (int, error) {
+	if timeout != 0 {
+		err := conn.SetDeadline(time.Now().Add(timeout))
+		if err != nil {
+			return 0, err
+		}
+	}
+
+	var succeed int
+
+	for i := 0; i < num; i++ {
+		if _, err := conn.Write(nil); err != nil && err != io.EOF {
+			fmt.Println(err)
+			continue
+		}
+
+		var buff = [2]byte{}
+		if _, err := conn.Read(buff[:]); err != nil {
+			fmt.Println(err)
+			continue
+		}
+
+		if string(buff[:]) != "ok" {
+			fmt.Println("receive unexpect response: ", string(buff[:]))
+			continue
+		}
+
+		succeed++
+		fmt.Printf("connection sussess to server %s\n", conn.RemoteAddr())
+	}
+
+	return succeed, nil
+}