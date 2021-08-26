1e402c7e3950041a84b838dd4bcd4ec4bf6d8f46
zwtop <wang.zhan@smartx.com>
2021-08-26 17:30:36 +0800

    refactor(e2e): init default tier when setup e2e
    Signed-off-by: zwtop <wang.zhan@smartx.com>

--- tests/e2e/cases/e2e_test.go
+++ tests/e2e/cases/e2e_test.go
@@ -27,7 +27,6 @@ import (
 	. "github.com/onsi/gomega"
 	"k8s.io/klog"
 
-	"github.com/smartxworks/lynx/pkg/apis/security/v1alpha1"
 	"github.com/smartxworks/lynx/tests/e2e/framework"
 )
 
@@ -54,9 +53,6 @@ var _ = BeforeSuite(func() {
 	// reset resource before start e2e
 	Expect(e2eEnv.ResetResource(ctx)).ToNot(HaveOccurred())
 
-	err = e2eEnv.SetupObjects(ctx, defaultTier(tier0), defaultTier(tier1), defaultTier(tier2))
-	Expect(err).ToNot(HaveOccurred())
-
 	// Will random restart controller and agent when e2e. Skip restart controller when provider
 	// is netns, because it may cause failed when create endpoint (failed to call webhook).
 	restarter := e2eEnv.NodeManager().ServiceRestarter(10, 30, e2eEnv.EndpointManager().Name() == "netns")
@@ -64,9 +60,6 @@ var _ = BeforeSuite(func() {
 })
 
 var _ = AfterSuite(func() {
-	err := e2eEnv.CleanObjects(ctx, defaultTier(tier0), defaultTier(tier1), defaultTier(tier2))
-	Expect(err).ToNot(HaveOccurred())
-
 	klog.Infof("complete all e2e test cases use %s", time.Since(startTime))
 	klog.Infof("run e2e-reset.sh to clean test environment")
 })
@@ -77,22 +70,3 @@ const (
 	tier1 = "tier1"
 	tier2 = "tier2"
 )
-
-func defaultTier(name string) *v1alpha1.Tier {
-	tier := &v1alpha1.Tier{}
-	tier.Name = name
-	tier.Spec.TierMode = v1alpha1.TierWhiteList
-
-	switch name {
-	case tier0:
-		tier.Spec.Priority = 0
-	case tier1:
-		tier.Spec.Priority = 1
-	case tier2:
-		tier.Spec.Priority = 2
-	default:
-		klog.Fatalf("tier %s is not default tier", name)
-	}
-
-	return tier
-}
