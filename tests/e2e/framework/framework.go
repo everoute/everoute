0a6c1b12cf1289baf19d8f6a8f6f5af39e642678
zwtop <wang.zhan@smartx.com>
2021-09-01 18:52:19 +0800

    fix(policy-controller): handle security policy reference endpoint namespace
    Endpoint in security policy should have the same namespace with the security policy
    
    Signed-off-by: zwtop <wang.zhan@smartx.com>

--- tests/e2e/framework/framework.go
+++ tests/e2e/framework/framework.go
@@ -113,7 +113,7 @@ func (f *Framework) CleanObjects(ctx context.Context, objects ...metav1.Object) 
 		}
 
 		err = wait.Poll(f.Interval(), f.Timeout(), func() (done bool, err error) {
-			var objKey = types.NamespacedName{Name: object.GetName()}
+			var objKey = types.NamespacedName{Name: object.GetName(), Namespace: object.GetNamespace()}
 			var obj = object.(runtime.Object)
 			var getErr = f.kubeClient.Get(ctx, objKey, obj.DeepCopyObject())
 			return errors.IsNotFound(getErr), nil
