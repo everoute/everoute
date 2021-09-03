f9afd9c1ba7cec78096c6974bcea06c1b4cffcc8
wu-cl <changliang.wu@smartx.com>
2021-09-03 10:58:41 +0800

    feat(agent): add new controller to update route table & iptables
    this controller will watch Node and then update network config, Meanwhile, updating will execute
    automatically every 100 seconds.

---
+++ pkg/agent/proxy/route.go
@@ -0,0 +1,311 @@
+/*
+Copyright 2021 The Lynx Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package proxy
+
+import (
+	"context"
+	"fmt"
+	"net"
+	"os"
+	"strings"
+	"sync"
+	"time"
+
+	"github.com/coreos/go-iptables/iptables"
+	"github.com/vishvananda/netlink"
+	"golang.org/x/sys/unix"
+	corev1 "k8s.io/api/core/v1"
+	"k8s.io/apimachinery/pkg/runtime"
+	"k8s.io/klog"
+	ctrl "sigs.k8s.io/controller-runtime"
+	"sigs.k8s.io/controller-runtime/pkg/client"
+	"sigs.k8s.io/controller-runtime/pkg/controller"
+	"sigs.k8s.io/controller-runtime/pkg/handler"
+	"sigs.k8s.io/controller-runtime/pkg/source"
+
+	"github.com/smartxworks/lynx/pkg/constants"
+)
+
+const (
+	defaultRouteTable = 254
+)
+
+// NodeReconciler watch node and update route table
+type NodeReconciler struct {
+	client.Client
+	Scheme *runtime.Scheme
+
+	StopChan    <-chan struct{}
+	updateMutex sync.Mutex
+}
+
+func GetNodeInternalIP(node corev1.Node) net.IP {
+	for _, item := range node.Status.Addresses {
+		if item.Type == corev1.NodeInternalIP {
+			return net.ParseIP(item.Address)
+		}
+	}
+	return nil
+}
+
+func GetRouteByDst(dst *net.IPNet) []netlink.Route {
+	var ret []netlink.Route
+	// List all route item in current node
+	routeList, err := netlink.RouteList(nil, unix.AF_INET)
+	if err != nil {
+		klog.Errorf("List route table error, err:%s", err)
+		return ret
+	}
+	for _, item := range routeList {
+		if item.Dst != nil && item.Dst.String() == dst.String() {
+			ret = append(ret, item)
+		}
+	}
+	return ret
+}
+
+func RouteEqual(r1, r2 netlink.Route) bool {
+	return ((r1.Dst == nil && r2.Dst == nil) ||
+		(r1.Dst != nil && r2.Dst != nil && r1.Dst.String() == r2.Dst.String())) &&
+		r1.Src.Equal(r2.Src) &&
+		r1.Gw.Equal(r2.Gw)
+}
+
+// UpdateRoute will be called when Node has been updated, or every 100 seconds.
+// This function will update route table in linux kernel. Old related route items which are useless will be deleted,
+// and new route item will be added.
+// In each node, there will be (n-1) * mi route items, n means the num of nodes, mi means m pod cidrs in node i.
+// for example, if there are two nodes in cluster and node2 has two pod cidrs, Here are route item in node1:
+// ip route add node2-podCIDR-1 via node2
+// ip route add node2-podCIDR-2 via node2
+func UpdateRoute(nodeList corev1.NodeList, thisNode corev1.Node) {
+	var oldRoute []netlink.Route
+	var targetRoute []netlink.Route
+	var err error
+
+	for _, item := range nodeList.Items {
+		// ignore current node
+		if item.Name == thisNode.Name {
+			continue
+		}
+		gw := GetNodeInternalIP(item)
+		if gw == nil {
+			klog.Errorf("Fail to get node internal IP in node: %s", item.Name)
+			continue
+		}
+		// multi-podCIDRs will create multi-routeItem
+		for _, podCIDR := range item.Spec.PodCIDRs {
+			dst, err := netlink.ParseIPNet(podCIDR)
+			if err != nil {
+				klog.Errorf("Parse podCIDR %s failed, err: %s", podCIDR, err)
+			}
+			tempRoute := GetRouteByDst(dst)
+			if len(tempRoute) != 0 {
+				oldRoute = append(oldRoute, tempRoute...)
+			}
+			targetRoute = append(targetRoute, netlink.Route{
+				Dst:   dst,
+				Gw:    gw,
+				Table: defaultRouteTable,
+			})
+		}
+	}
+
+	var delRoute []netlink.Route
+	targetRouteExist := make([]bool, len(targetRoute))
+	// calculate route to add and delete
+	for _, oldItem := range oldRoute {
+		exist := false
+		for i, newItem := range targetRoute {
+			if RouteEqual(oldItem, newItem) {
+				exist = true
+				targetRouteExist[i] = true
+				break
+			}
+		}
+		if !exist {
+			delRoute = append(delRoute, oldItem)
+		}
+	}
+
+	// add & del route
+	for i := range delRoute {
+		if err = netlink.RouteDel(&delRoute[i]); err != nil {
+			klog.Errorf("delete route item failed, err: %s", err)
+		} else {
+			klog.Infof("delete route item %s", &delRoute[i])
+		}
+	}
+	for i := range targetRoute {
+		// skip existed route
+		if targetRouteExist[i] {
+			continue
+		}
+		if err = netlink.RouteAdd(&targetRoute[i]); err != nil {
+			klog.Errorf("[ALERT] add route item failed, err: %s", err)
+		} else {
+			klog.Infof("add route item %s", &targetRoute[i])
+		}
+	}
+}
+
+// UpdateIptables will be called when Node has been updated, or every 100 seconds.
+// This function will update iptables in linux kernel.
+// iptables used to DNAT for the OUTPUT traffic( outside of the cluster)
+func UpdateIptables(nodeList corev1.NodeList, thisNode corev1.Node) {
+	var exist bool
+	var err error
+
+	ipt, err := iptables.New()
+	if err != nil {
+		klog.Errorf("init iptables error, err: %s", err)
+		return
+	}
+
+	// TODO: LYNX name needs to update
+	// check existence of chain LYNX-OUTPUT, if not, then create it
+	if exist, err = ipt.ChainExists("nat", "LYNX-OUTPUT"); err != nil {
+		klog.Errorf("Get iptables LYNX-OUTPUT error, error: %s", err)
+		return
+	}
+	if !exist {
+		err = ipt.NewChain("nat", "LYNX-OUTPUT")
+		if err != nil {
+			klog.Errorf("Create iptables LYNX-OUTPUT error, error: %s", err)
+			return
+		}
+	}
+
+	// check and add LYNX-OUTPUT to POSTROUTING
+	if exist, err = ipt.Exists("nat", "POSTROUTING", "-j", "LYNX-OUTPUT"); err != nil {
+		klog.Errorf("Check LYNX-OUTPUT in nat POSTROUTING error, err: %s", err)
+	}
+	if !exist {
+		if err = ipt.Append("nat", "POSTROUTING", "-j", "LYNX-OUTPUT"); err != nil {
+			klog.Errorf("Append LYNX-OUTPUT into nat POSTROUTING error, err: %s", err)
+		}
+	}
+
+	// check and add MASQUERADE in LYNX-OUTPUT"
+	for _, podCIDR := range thisNode.Spec.PodCIDRs {
+		ruleSpec := []string{"-s", podCIDR, "-j", "MASQUERADE"}
+		if exist, err = ipt.Exists("nat", "LYNX-OUTPUT", ruleSpec...); err != nil {
+			klog.Errorf("Check MASQUERADE rule in nat LYNX-OUTPUT error, rule: %s, err: %s", ruleSpec, err)
+			continue
+		}
+		if !exist {
+			err = ipt.Append("nat", "LYNX-OUTPUT", ruleSpec...)
+			if err != nil {
+				klog.Errorf("Add MASQUERADE rule in nat LYNX-OUTPUT error, rule: %s, err: %s", ruleSpec, err)
+				continue
+			}
+		}
+	}
+
+	// check and add ACCEPT in LYNX-OUTPUT
+	// ACCEPT is used to skip the traffic inside the cluster.
+	for _, podCIDR := range thisNode.Spec.PodCIDRs {
+		for _, nodeItem := range nodeList.Items {
+			if nodeItem.Name == thisNode.Name {
+				continue
+			}
+			for _, otherPodCIDR := range nodeItem.Spec.PodCIDRs {
+				ruleSpec := []string{"-s", podCIDR, "-d", otherPodCIDR, "-j", "ACCEPT"}
+				if exist, err = ipt.Exists("nat", "LYNX-OUTPUT", ruleSpec...); err != nil {
+					klog.Errorf("Check ACCEPT rule in nat LYNX-OUTPUT error, rule: %s, err: %s", ruleSpec, err)
+					continue
+				}
+				if !exist {
+					if err = ipt.Insert("nat", "LYNX-OUTPUT", 1, ruleSpec...); err != nil {
+						klog.Errorf("[ALERT] Add ACCEPT rule in nat LYNX-OUTPUT error, rule: %s, err: %s", ruleSpec, err)
+						continue
+					}
+				}
+			}
+		}
+	}
+}
+
+func (r *NodeReconciler) UpdateNetwork() {
+	klog.Infof("update network config")
+	r.updateMutex.Lock()
+	defer r.updateMutex.Unlock()
+
+	// List all nodes in cluster
+	nodeList := corev1.NodeList{}
+	if err := r.List(context.Background(), &nodeList); err != nil {
+		klog.Errorf("List Node error, err: %s", err)
+		return
+	}
+	// Get current node
+	nodeName, _ := os.Hostname()
+	nodeName = strings.ToLower(nodeName)
+	var currentNode corev1.Node
+	for _, item := range nodeList.Items {
+		if item.Name == nodeName {
+			currentNode = item
+			break
+		}
+	}
+
+	UpdateRoute(nodeList, currentNode)
+	UpdateIptables(nodeList, currentNode)
+}
+
+// Reconcile receive node from work queue, synchronize network config
+func (r *NodeReconciler) Reconcile(req ctrl.Request) (ctrl.Result, error) {
+	klog.Infof("NodeReconciler received node %s reconcile", req.NamespacedName)
+
+	r.UpdateNetwork()
+
+	return ctrl.Result{}, nil
+}
+
+// SetupWithManager create and add Endpoint Controller to the manager.
+func (r *NodeReconciler) SetupWithManager(mgr ctrl.Manager) error {
+	if mgr == nil {
+		return fmt.Errorf("can't setup with nil manager")
+	}
+
+	c, err := controller.New("node-controller", mgr, controller.Options{
+		MaxConcurrentReconciles: constants.DefaultMaxConcurrentReconciles,
+		Reconciler:              r,
+	})
+	if err != nil {
+		return err
+	}
+
+	err = c.Watch(&source.Kind{Type: &corev1.Node{}}, &handler.EnqueueRequestForObject{})
+	if err != nil {
+		return err
+	}
+
+	// update network config every 100 seconds
+	ticker := time.NewTicker(100 * time.Second)
+	go func() {
+		for {
+			select {
+			case <-ticker.C:
+				r.UpdateNetwork()
+			case <-r.StopChan:
+				return
+			}
+		}
+	}()
+
+	return nil
+}
