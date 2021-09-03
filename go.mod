0ce434746949632f6555848233925574cac2d816
wu-cl <changliang.wu@smartx.com>
2021-09-03 10:58:41 +0800

    feat(agent): add new controller to update route table & iptables
    this controller will watch Node and then update network config, Meanwhile, updating will execute
    automatically every 100 seconds.

--- go.mod
+++ go.mod
@@ -5,8 +5,11 @@ go 1.15
 require (
 	github.com/99designs/gqlgen v0.13.0
 	github.com/cenk/hub v1.0.1 // indirect
+	github.com/containernetworking/cni v1.0.0
+	github.com/containernetworking/plugins v1.0.0
 	github.com/contiv/libovsdb v0.0.0
 	github.com/contiv/ofnet v0.0.0-00010101000000-000000000000
+	github.com/coreos/go-iptables v0.6.0
 	github.com/fatih/color v1.7.0
 	github.com/gertd/go-pluralize v0.1.7
 	github.com/go-logr/logr v0.4.0 // indirect
@@ -15,18 +18,20 @@ require (
 	github.com/gorilla/websocket v1.4.2
 	github.com/hashicorp/go-retryablehttp v0.7.0
 	github.com/j-keck/arping v1.0.2
-	github.com/onsi/ginkgo v1.13.0
-	github.com/onsi/gomega v1.10.1
+	github.com/onsi/ginkgo v1.16.4
+	github.com/onsi/gomega v1.15.0
 	github.com/spf13/cobra v1.1.1
 	github.com/vektah/gqlparser/v2 v2.1.0
-	golang.org/x/crypto v0.0.0-20201002170205-7f63de1d35b0
-	golang.org/x/sys v0.0.0-20201112073958-5cba982894dd
+	github.com/vishvananda/netlink v1.1.1-0.20210330154013-f5de75959ad5
+	golang.org/x/crypto v0.0.0-20210322153248-0c34fe9e7dc2
+	golang.org/x/sys v0.0.0-20210809222454-d867a43fc93e
 	google.golang.org/grpc v1.35.0
-	gopkg.in/yaml.v2 v2.3.0
-	k8s.io/api v0.20.1
-	k8s.io/apimachinery v0.20.1
+	google.golang.org/protobuf v1.26.0
+	gopkg.in/yaml.v2 v2.4.0
+	k8s.io/api v0.20.6
+	k8s.io/apimachinery v0.20.6
 	k8s.io/cli-runtime v0.20.1
-	k8s.io/client-go v0.20.1
+	k8s.io/client-go v0.20.6
 	k8s.io/klog v1.0.0
 	k8s.io/kube-openapi v0.0.0-20201113171705-d219536bb9fd
 	k8s.io/utils v0.0.0-20201110183641-67b214c5f920
@@ -36,6 +41,6 @@ require (
 replace (
 	github.com/contiv/libOpenflow => github.com/echkenluo/libOpenflow v0.0.0-20210415080703-4361a8a1982d
 	github.com/contiv/libovsdb => github.com/smartxworks/libovsdb v0.0.0-20210326110222-6c508538aa65
-	github.com/contiv/ofnet => github.com/smartxworks/ofnet v0.0.0-20210728112237-9d1e0ceb2424
+	github.com/contiv/ofnet => github.com/smartxworks/ofnet v0.0.0-20210903030657-a5b8731936a3
 	github.com/osrg/gobgp => github.com/zwtop/gobgp v0.0.0-20210127101833-12edfc1f4514
 )
---
+++ pkg/agent/proxy/route_test.go
@@ -0,0 +1,93 @@
+/*
+Copyright 2021 The Lynx Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package proxy
+
+import (
+	"net"
+
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+	"github.com/vishvananda/netlink"
+	corev1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+)
+
+var _ = Describe("Test route.go", func() {
+	var node1 *corev1.Node
+	var route1, route2, route3, route4, route5 netlink.Route
+	BeforeEach(func() {
+		node1 = &corev1.Node{
+			ObjectMeta: metav1.ObjectMeta{
+				Name: "node1",
+			},
+			Spec: corev1.NodeSpec{
+				PodCIDR:  "10.244.1.0/24",
+				PodCIDRs: []string{"10.244.1.0/24"},
+			},
+			Status: corev1.NodeStatus{
+				Addresses: []corev1.NodeAddress{{
+					Type:    corev1.NodeInternalIP,
+					Address: "192.168.1.1",
+				}, {
+					Type:    corev1.NodeHostName,
+					Address: "node1",
+				}},
+			}}
+
+		_, dst1, _ := net.ParseCIDR("10.244.1.0/24")
+		_, dst2, _ := net.ParseCIDR("10.244.2.0/24")
+		route1 = netlink.Route{
+			Src: net.ParseIP("192.168.2.100"),
+			Dst: dst2,
+			Gw:  net.ParseIP("192.168.2.1"),
+		}
+		route2 = netlink.Route{
+			Src: net.ParseIP("192.168.2.100"),
+			Dst: dst1,
+			Gw:  net.ParseIP("192.168.2.1"),
+		}
+		route3 = netlink.Route{
+			Src: net.ParseIP("192.168.2.101"),
+			Dst: dst1,
+			Gw:  net.ParseIP("192.168.2.1"),
+		}
+		route4 = netlink.Route{
+			Src: net.ParseIP("192.168.2.100"),
+			Dst: dst2,
+			Gw:  net.ParseIP("192.168.2.1"),
+		}
+		route5 = netlink.Route{
+			Src: net.ParseIP("192.168.2.100"),
+			Dst: dst1,
+			Gw:  net.ParseIP("192.168.2.2"),
+		}
+
+	})
+
+	It("Test GetNodeInternalIP", func() {
+		ret := GetNodeInternalIP(*node1)
+		Expect(ret).Should(Equal("192.168.1.1"))
+	})
+
+	It("Test RouteEqual", func() {
+		Expect(RouteEqual(route1, route2)).Should(BeTrue())
+		Expect(RouteEqual(route1, route3)).Should(BeFalse())
+		Expect(RouteEqual(route1, route4)).Should(BeFalse())
+		Expect(RouteEqual(route1, route5)).Should(BeFalse())
+	})
+
+})
