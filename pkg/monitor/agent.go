385fbaaf8ad5d46a0893762bb86bc9cc32516ef3
cl1111 <834980140@qq.com>
2022-03-16 15:04:26 +0800

    Use mac_in_use to add non-vm interface as endpoint

--- pkg/monitor/agent.go
+++ pkg/monitor/agent.go
@@ -455,8 +455,8 @@ func (monitor *AgentMonitor) filterEndpointUpdated(rowupdate ovsdb.RowUpdate) (*
 func (monitor *AgentMonitor) filterEndpointAdded(rowupdate ovsdb.RowUpdate) *datapath.Endpoint {
 	var macStr string
 	newExternalIds := rowupdate.New.Fields["external_ids"].(ovsdb.OvsMap).GoMap
-	newMacInUse, ok := rowupdate.New.Fields["mac_in_use"].(string)
-	_, ok = newExternalIds[LocalEndpointIdentity]
+	newMacInUse, _ := rowupdate.New.Fields["mac_in_use"].(string)
+	_, ok := newExternalIds[LocalEndpointIdentity]
 	if !ok {
 		macStr = newMacInUse
 	} else {
