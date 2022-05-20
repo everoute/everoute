621d88fb4eb166d216adab9a009bb7ba6e400452
zwtop <wang.zhan@smartx.com>
2022-05-20 13:51:18 +0800

    fix(datapath): fix fetch ofport from interface
    Signed-off-by: zwtop <wang.zhan@smartx.com>

--- pkg/agent/datapath/utils.go
+++ pkg/agent/datapath/utils.go
@@ -27,51 +27,55 @@ const (
 		set -o nounset
 		set -o xtrace
 
-        DEFAULT_BRIDGE=%s
-        LOCAL_TO_POLICY_OFPORT=101
-        POLICY_TO_LOCAL_OFPORT=102
-        POLICY_TO_CLS_OFPORT=201
-        CLS_TO_POLICY_OFPORT=202
-        CLS_TO_UPLINK_OFPORT=301
-        UPLINK_TO_CLS_OFPORT=302
+		DEFAULT_BRIDGE=%s
+		POLICY_BRIDGE="${DEFAULT_BRIDGE}-policy"
+		CLS_BRIDGE="${DEFAULT_BRIDGE}-cls"
+		UPLINK_BRIDGE="${DEFAULT_BRIDGE}-uplink"
 
-        LOCAL_TO_POLICY_PATCH=$DEFAULT_BRIDGE-"local-to-policy"
-        POLICY_TO_LOCAL_PATCH=$DEFAULT_BRIDGE-"policy-to-local"
-        POLICY_TO_CLS_PATCH=$DEFAULT_BRIDGE-"policy-to-cls"
-        CLS_TO_POLICY_PATCH=$DEFAULT_BRIDGE-"cls-to-policy"
-        CLS_TO_UPLINK_PATCH=$DEFAULT_BRIDGE-"cls-to-uplink"
-        UPLINK_TO_CLS_PATCH=$DEFAULT_BRIDGE-"uplink-to-cls"
+		LOCAL_TO_POLICY_PATCH="${DEFAULT_BRIDGE}-local-to-policy"
+		POLICY_TO_LOCAL_PATCH="${POLICY_BRIDGE}-policy-to-local"
+		POLICY_TO_CLS_PATCH="${POLICY_BRIDGE}-policy-to-cls"
+		CLS_TO_POLICY_PATCH="${CLS_BRIDGE}-cls-to-policy"
+		CLS_TO_UPLINK_PATCH="${CLS_BRIDGE}-cls-to-uplink"
+		UPLINK_TO_CLS_PATCH="${UPLINK_BRIDGE}-uplink-to-cls"
 
-        echo "add bridge chain and uplink port"
-        ovs-vsctl add-br ${DEFAULT_BRIDGE}
-        ovs-vsctl add-br ${DEFAULT_BRIDGE}-policy
-        ovs-vsctl add-br ${DEFAULT_BRIDGE}-cls
-        ovs-vsctl add-br ${DEFAULT_BRIDGE}-uplink
+		echo "add bridge chain and uplink port"
+		ovs-vsctl add-br ${DEFAULT_BRIDGE}
+		ovs-vsctl add-br ${POLICY_BRIDGE}
+		ovs-vsctl add-br ${CLS_BRIDGE}
+		ovs-vsctl add-br ${UPLINK_BRIDGE}
 
-        ovs-vsctl \
-            -- add-port $DEFAULT_BRIDGE $LOCAL_TO_POLICY_PATCH \
-            -- set interface $LOCAL_TO_POLICY_PATCH type=patch options:peer=$POLICY_TO_LOCAL_PATCH ofport=$LOCAL_TO_POLICY_OFPORT \
-            -- add-port ${DEFAULT_BRIDGE}-policy $POLICY_TO_LOCAL_PATCH \
-            -- set interface $POLICY_TO_LOCAL_PATCH type=patch options:peer=$LOCAL_TO_POLICY_PATCH ofport=$POLICY_TO_LOCAL_OFPORT
+		ovs-vsctl \
+		    -- add-port ${DEFAULT_BRIDGE} ${LOCAL_TO_POLICY_PATCH} \
+		    -- set interface ${LOCAL_TO_POLICY_PATCH} type=patch options:peer=${POLICY_TO_LOCAL_PATCH} \
+		    -- add-port ${POLICY_BRIDGE} ${POLICY_TO_LOCAL_PATCH} \
+		    -- set interface ${POLICY_TO_LOCAL_PATCH} type=patch options:peer=${LOCAL_TO_POLICY_PATCH}
 
-        ovs-vsctl \
-            -- add-port ${DEFAULT_BRIDGE}-policy $POLICY_TO_CLS_PATCH \
-            -- set interface $POLICY_TO_CLS_PATCH type=patch options:peer=$CLS_TO_POLICY_PATCH ofport=$POLICY_TO_CLS_OFPORT\
-            -- add-port ${DEFAULT_BRIDGE}-cls $CLS_TO_POLICY_PATCH \
-            -- set interface $CLS_TO_POLICY_PATCH type=patch options:peer=$POLICY_TO_CLS_PATCH ofport=$CLS_TO_POLICY_OFPORT
+		ovs-vsctl \
+		    -- add-port ${POLICY_BRIDGE} ${POLICY_TO_CLS_PATCH} \
+		    -- set interface ${POLICY_TO_CLS_PATCH} type=patch options:peer=${CLS_TO_POLICY_PATCH} \
+		    -- add-port ${CLS_BRIDGE} ${CLS_TO_POLICY_PATCH} \
+		    -- set interface ${CLS_TO_POLICY_PATCH} type=patch options:peer=${POLICY_TO_CLS_PATCH} 
 
-        ovs-vsctl \
-            -- add-port ${DEFAULT_BRIDGE}-uplink $UPLINK_TO_CLS_PATCH \
-            -- set interface $UPLINK_TO_CLS_PATCH type=patch options:peer=$CLS_TO_UPLINK_PATCH ofport=$UPLINK_TO_CLS_OFPORT \
-            -- add-port ${DEFAULT_BRIDGE}-cls $CLS_TO_UPLINK_PATCH \
-            -- set interface $CLS_TO_UPLINK_PATCH type=patch options:peer=$UPLINK_TO_CLS_PATCH ofport=$CLS_TO_UPLINK_OFPORT
+		ovs-vsctl \
+		    -- add-port ${UPLINK_BRIDGE} ${UPLINK_TO_CLS_PATCH} \
+		    -- set interface ${UPLINK_TO_CLS_PATCH} type=patch options:peer=${CLS_TO_UPLINK_PATCH} \
+		    -- add-port ${CLS_BRIDGE} ${CLS_TO_UPLINK_PATCH} \
+		    -- set interface ${CLS_TO_UPLINK_PATCH} type=patch options:peer=${UPLINK_TO_CLS_PATCH} 
 
-        ovs-ofctl add-flow ${DEFAULT_BRIDGE}-uplink "table=0,priority=10,actions=normal"
+		ovs-ofctl add-flow ${UPLINK_BRIDGE} "table=0,priority=10,actions=normal"
     `
 	CleanBridgeChain = `
-        DEFAULT_BRIDGE=%s
-        ovs-vsctl del-br ${DEFAULT_BRIDGE} && ovs-vsctl del-br ${DEFAULT_BRIDGE}-policy && ovs-vsctl del-br \
-            ${DEFAULT_BRIDGE}-cls && ovs-vsctl del-br ${DEFAULT_BRIDGE}-uplink
+		DEFAULT_BRIDGE=%s
+		POLICY_BRIDGE="${DEFAULT_BRIDGE}-policy"
+		CLS_BRIDGE="${DEFAULT_BRIDGE}-cls"
+		UPLINK_BRIDGE="${DEFAULT_BRIDGE}-uplink"
+
+		ovs-vsctl \
+			-- del-br ${DEFAULT_BRIDGE} \
+			-- del-br ${POLICY_BRIDGE} \
+			-- del-br ${CLS_BRIDGE} \
+			-- del-br ${UPLINK_BRIDGE}
     `
 )
 
--- tests/e2e/scripts/agent-setup.sh
+++ tests/e2e/scripts/agent-setup.sh
@@ -20,47 +20,51 @@ set -o nounset
 
 UPLINK_IFACE=${1:-ens11}
 KUBECONFIG_PATH=${2:-/var/lib/everoute/agent-kubeconfig.yaml}
-DEFAULT_BRIDGE="ovsbr1"
 OFPORT_NUM=10
 AGENT_CONFIG_PATH=/var/lib/everoute/agentconfig.yaml
 AGENT_NAME_PATH=/var/lib/everoute/agent/name
 
-LOCAL_TO_POLICY_PATCH=$DEFAULT_BRIDGE"-local-to-policy"
-POLICY_TO_LOCAL_PATCH=$DEFAULT_BRIDGE"-policy-to-local"
-POLICY_TO_CLS_PATCH=$DEFAULT_BRIDGE"-policy-to-cls"
-CLS_TO_POLICY_PATCH=$DEFAULT_BRIDGE"-cls-to-policy"
-CLS_TO_UPLINK_PATCH=$DEFAULT_BRIDGE"-cls-to-uplink"
-UPLINK_TO_CLS_PATCH=$DEFAULT_BRIDGE"-uplink-to-cls"
+DEFAULT_BRIDGE="ovsbr1"
+POLICY_BRIDGE="${DEFAULT_BRIDGE}-policy"
+CLS_BRIDGE="${DEFAULT_BRIDGE}-cls"
+UPLINK_BRIDGE="${DEFAULT_BRIDGE}-uplink"
+
+LOCAL_TO_POLICY_PATCH="${DEFAULT_BRIDGE}-local-to-policy"
+POLICY_TO_LOCAL_PATCH="${POLICY_BRIDGE}-policy-to-local"
+POLICY_TO_CLS_PATCH="${POLICY_BRIDGE}-policy-to-cls"
+CLS_TO_POLICY_PATCH="${CLS_BRIDGE}-cls-to-policy"
+CLS_TO_UPLINK_PATCH="${CLS_BRIDGE}-cls-to-uplink"
+UPLINK_TO_CLS_PATCH="${UPLINK_BRIDGE}-uplink-to-cls"
 
 echo "add uplink interface if not exists"
-ip link show ${UPLINK_IFACE} || ip link add ${UPLINK_IFACE} type bridge
+ip link show "${UPLINK_IFACE}" || ip link add "${UPLINK_IFACE}" type bridge
 
 echo "add bridge chain and uplink port"
 ovs-vsctl add-br ${DEFAULT_BRIDGE} -- set bridge ${DEFAULT_BRIDGE} protocols=OpenFlow10,OpenFlow11,OpenFlow12,OpenFlow13 fail_mode=secure
-ovs-vsctl add-br ${DEFAULT_BRIDGE}-policy -- set bridge ${DEFAULT_BRIDGE}-policy protocols=OpenFlow10,OpenFlow11,OpenFlow12,OpenFlow13 fail_mode=secure
-ovs-vsctl add-br ${DEFAULT_BRIDGE}-cls -- set bridge ${DEFAULT_BRIDGE}-cls protocols=OpenFlow10,OpenFlow11,OpenFlow12,OpenFlow13 fail_mode=secure
-ovs-vsctl add-br ${DEFAULT_BRIDGE}-uplink -- set bridge ${DEFAULT_BRIDGE}-uplink protocols=OpenFlow10,OpenFlow11,OpenFlow12,OpenFlow13 fail_mode=secure
+ovs-vsctl add-br ${POLICY_BRIDGE} -- set bridge ${POLICY_BRIDGE} protocols=OpenFlow10,OpenFlow11,OpenFlow12,OpenFlow13 fail_mode=secure
+ovs-vsctl add-br ${CLS_BRIDGE} -- set bridge ${CLS_BRIDGE} protocols=OpenFlow10,OpenFlow11,OpenFlow12,OpenFlow13 fail_mode=secure
+ovs-vsctl add-br ${UPLINK_BRIDGE} -- set bridge ${UPLINK_BRIDGE} protocols=OpenFlow10,OpenFlow11,OpenFlow12,OpenFlow13 fail_mode=secure
 
 ovs-vsctl \
-    -- add-port $DEFAULT_BRIDGE $LOCAL_TO_POLICY_PATCH \
-    -- set interface $LOCAL_TO_POLICY_PATCH type=patch options:peer=$POLICY_TO_LOCAL_PATCH \
-    -- add-port ${DEFAULT_BRIDGE}-policy $POLICY_TO_LOCAL_PATCH \
-    -- set interface $POLICY_TO_LOCAL_PATCH type=patch options:peer=$LOCAL_TO_POLICY_PATCH
+    -- add-port ${DEFAULT_BRIDGE} ${LOCAL_TO_POLICY_PATCH} \
+    -- set interface ${LOCAL_TO_POLICY_PATCH} type=patch options:peer=${POLICY_TO_LOCAL_PATCH} \
+    -- add-port ${POLICY_BRIDGE} ${POLICY_TO_LOCAL_PATCH} \
+    -- set interface ${POLICY_TO_LOCAL_PATCH} type=patch options:peer=${LOCAL_TO_POLICY_PATCH}
 
 ovs-vsctl \
-    -- add-port ${DEFAULT_BRIDGE}-policy $POLICY_TO_CLS_PATCH \
-    -- set interface $POLICY_TO_CLS_PATCH type=patch options:peer=$CLS_TO_POLICY_PATCH \
-    -- add-port ${DEFAULT_BRIDGE}-cls $CLS_TO_POLICY_PATCH \
-    -- set interface $CLS_TO_POLICY_PATCH type=patch options:peer=$POLICY_TO_CLS_PATCH 
+    -- add-port ${POLICY_BRIDGE} ${POLICY_TO_CLS_PATCH} \
+    -- set interface ${POLICY_TO_CLS_PATCH} type=patch options:peer=${CLS_TO_POLICY_PATCH} \
+    -- add-port ${CLS_BRIDGE} ${CLS_TO_POLICY_PATCH} \
+    -- set interface ${CLS_TO_POLICY_PATCH} type=patch options:peer=${POLICY_TO_CLS_PATCH}
 
 ovs-vsctl \
-    -- add-port ${DEFAULT_BRIDGE}-uplink $UPLINK_TO_CLS_PATCH \
-    -- set interface $UPLINK_TO_CLS_PATCH type=patch options:peer=$CLS_TO_UPLINK_PATCH \
-    -- add-port ${DEFAULT_BRIDGE}-cls $CLS_TO_UPLINK_PATCH \
-    -- set interface $CLS_TO_UPLINK_PATCH type=patch options:peer=$UPLINK_TO_CLS_PATCH 
+    -- add-port ${UPLINK_BRIDGE} ${UPLINK_TO_CLS_PATCH} \
+    -- set interface ${UPLINK_TO_CLS_PATCH} type=patch options:peer=${CLS_TO_UPLINK_PATCH} \
+    -- add-port ${CLS_BRIDGE} ${CLS_TO_UPLINK_PATCH} \
+    -- set interface ${CLS_TO_UPLINK_PATCH} type=patch options:peer=${UPLINK_TO_CLS_PATCH}
 
-ovs-vsctl add-port ${DEFAULT_BRIDGE}-uplink ${UPLINK_IFACE} -- set Port ${UPLINK_IFACE} external_ids=uplink-port="true" -- set Interface ${UPLINK_IFACE} ofport=${OFPORT_NUM}
-ovs-ofctl add-flow ${DEFAULT_BRIDGE}-uplink "table=0,priority=10,actions=normal"
+ovs-vsctl add-port ${UPLINK_BRIDGE} "${UPLINK_IFACE}" -- set Port "${UPLINK_IFACE}" external_ids=uplink-port="true" -- set Interface "${UPLINK_IFACE}" ofport=${OFPORT_NUM}
+ovs-ofctl add-flow ${UPLINK_BRIDGE} "table=0,priority=10,actions=normal"
 
 echo "generate everoute-agent config"
 mkdir -p "$(dirname ${AGENT_CONFIG_PATH})"
@@ -72,4 +76,4 @@ mkdir -p "$(dirname ${AGENT_NAME_PATH})"
 cat /proc/sys/kernel/random/uuid > ${AGENT_NAME_PATH}
 
 echo "start everoute-agent"
-nohup /usr/local/bin/everoute-agent --kubeconfig ${KUBECONFIG_PATH} > /var/log/everoute-agent.log 2>&1 &
+nohup /usr/local/bin/everoute-agent --kubeconfig "${KUBECONFIG_PATH}" > /var/log/everoute-agent.log 2>&1 &
