66f13d75951bbef34bc91ed8a1590073fb9cdb66
Jiao Zhou <jiao.zhou@smartx.com>
2025-02-13 14:29:16 +0800

    feat: mount trafficredirect nic

---
+++ pkg/trafficredirect/action/deploy.go
@@ -0,0 +1,105 @@
+package action
+
+import (
+	"k8s.io/apimachinery/pkg/util/sets"
+	"k8s.io/klog/v2"
+
+	"github.com/everoute/everoute/pkg/config"
+	"github.com/everoute/everoute/pkg/trafficredirect/types"
+)
+
+func Reset(cfg *config.AgentConfig) error {
+	bridges, err := getAllBridge()
+	if err != nil {
+		return err
+	}
+	if bridges == nil {
+		bridges = sets.New[string]()
+	}
+
+	newCfg := make(map[string]*config.TRConfig, 0)
+	if cfg != nil && cfg.VdsConfigs != nil {
+		for vds := range cfg.VdsConfigs {
+			if len(cfg.VdsConfigs[vds].TrafficRedirects) == 0 {
+				continue
+			}
+			br := cfg.VdsConfigs[vds].BrideName
+			bridges.Insert(br)
+			// only support one
+			newCfg[br] = &cfg.VdsConfigs[vds].TrafficRedirects[0]
+		}
+	}
+
+	for _, br := range bridges.UnsortedList() {
+		if err := processVds(br, newCfg[br]); err != nil {
+			klog.Errorf("Failed to process ovs bridge %s trafficredirect nic, config is %v", br, newCfg[br])
+			return err
+		}
+		klog.Infof("Success to process ovs bridge %s trafficredirect nic, config is %v", br, newCfg[br])
+	}
+	return nil
+}
+
+func processVds(ovsbrName string, cfg *config.TRConfig) error {
+	if cfg == nil {
+		return ovsbrUnmountTrafficRedirect(ovsbrName)
+	}
+
+	oldNicIn, err := getNicConfig(ovsbrName, types.NicIn)
+	if err != nil {
+		return err
+	}
+	if oldNicIn != nil && oldNicIn.IfaceID != cfg.NicIn {
+		if err := ovsbrUnmountTrafficRedirect(ovsbrName, types.NicIn); err != nil {
+			return err
+		}
+	}
+	oldNicOut, err := getNicConfig(ovsbrName, types.NicOut)
+	if err != nil {
+		return err
+	}
+	if oldNicOut != nil && oldNicOut.IfaceID != cfg.NicOut {
+		if err := ovsbrUnmountTrafficRedirect(ovsbrName, types.NicOut); err != nil {
+			return err
+		}
+	}
+
+	ifaceName, err := findTrafficRedirectNic(ovsbrName, cfg.NicIn, types.NicIn)
+	if err != nil {
+		return err
+	}
+	if err := MountTRNic(ovsbrName, ifaceName, cfg.NicIn, types.NicIn); err != nil {
+		return err
+	}
+	klog.Infof("Success to mount trafficeredirect nic(ifaceID: %s) to ovs bridge %s direct %s", cfg.NicIn, ovsbrName, types.NicIn)
+
+	ifaceName, err = findTrafficRedirectNic(ovsbrName, cfg.NicOut, types.NicOut)
+	if err != nil {
+		return err
+	}
+	if err := MountTRNic(ovsbrName, ifaceName, cfg.NicOut, types.NicOut); err != nil {
+		return err
+	}
+	klog.Infof("Success to mount trafficeredirect nic(ifaceID: %s) to ovs bridge %s direct %s", cfg.NicIn, ovsbrName, types.NicIn)
+	return nil
+}
+
+func ovsbrUnmountTrafficRedirect(ovsbrName string, directs ...types.NicDirect) error {
+	// todo del flows
+	if len(directs) == 0 || directs[0] == types.NicIn {
+		if err := UnmountTRNic(ovsbrName, types.NicIn); err != nil {
+			klog.Errorf("Failed to unmount ovs bridge %s trafficredirect nic direct %s: %s", ovsbrName, types.NicIn, err)
+			return err
+		}
+		klog.Infof("Success to unmount ovs bridge %s trafficredirect nic direct %s", ovsbrName, types.NicIn)
+	}
+
+	if len(directs) == 0 || directs[0] == types.NicOut {
+		if err := UnmountTRNic(ovsbrName, types.NicOut); err != nil {
+			klog.Errorf("Failed to unmount ovs bridge %s trafficredirect nic direct %s: %s", ovsbrName, types.NicOut, err)
+			return err
+		}
+		klog.Infof("Success to unmount ovs bridge %s trafficredirect nic direct %s", ovsbrName, types.NicOut)
+	}
+	return nil
+}
---
+++ pkg/trafficredirect/action/deploy_test.go
@@ -0,0 +1,106 @@
+package action
+
+import (
+	"fmt"
+
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+
+	"github.com/everoute/everoute/pkg/config"
+	"github.com/everoute/everoute/pkg/trafficredirect/types"
+)
+
+var _ = Describe("Reset", func() {
+	Describe("deploy trafficredirect", func() {
+		cfg := &config.AgentConfig{
+			VdsConfigs: map[string]config.VdsConfig{
+				"vds1": {
+					BrideName: testBr1,
+					TrafficRedirects: []config.TRConfig{
+						{
+							NicIn:  ifaceID0,
+							NicOut: ifaceID1,
+						},
+					},
+				},
+			},
+		}
+
+		BeforeEach(func() {
+			cmd := fmt.Sprintf("ovs-vsctl add-br %s -- add-port %s %s -- set interface %s external_ids='%s'", svcChainBr, svcChainBr, tap0, tap0, externalIDs0)
+			_, err := excuteCommand(cmd)
+			g.Expect(err).ShouldNot(HaveOccurred())
+			cmd = fmt.Sprintf("ovs-vsctl -- add-port %s %s -- set interface %s external_ids='%s'", svcChainBr, tap1, tap1, externalIDs1)
+			_, err = excuteCommand(cmd)
+			g.Expect(err).ShouldNot(HaveOccurred())
+			cmd = fmt.Sprintf("ovs-vsctl add-br %s-policy", testBr1)
+			_, err = excuteCommand(cmd)
+			g.Expect(err).ShouldNot(HaveOccurred())
+		})
+
+		AfterEach(func() {
+			cmd := fmt.Sprintf("ovs-vsctl --if-exists del-br %s-policy", testBr1)
+			_, err := excuteCommand(cmd)
+			g.Expect(err).ShouldNot(HaveOccurred())
+			cmd = fmt.Sprintf("ovs-vsctl --if-exists del-br %s", svcChainBr)
+			_, err = excuteCommand(cmd)
+			g.Expect(err).ShouldNot(HaveOccurred())
+		})
+
+		It("run", func() {
+			By("deploy success")
+			err := Reset(cfg)
+			g.Expect(err).ShouldNot(HaveOccurred())
+
+			p, err := getPortInfo(tap0)
+			g.Expect(err).ShouldNot(HaveOccurred())
+			g.Expect(p).ShouldNot(BeNil())
+			g.Expect(p.brName).Should(Equal(testBr1 + "-policy"))
+			g.Expect(p.intfExternalIDs).Should(Equal(externalIDs0))
+			g.Expect(p.intfIfaceID).Should(Equal(ifaceID0))
+			c, err := getNicConfig(testBr1, types.NicIn)
+			g.Expect(err).ShouldNot(HaveOccurred())
+			g.Expect(c).ShouldNot(BeNil())
+			g.Expect(c.IfaceID).Should(Equal(ifaceID0))
+			g.Expect(c.PortName).Should(Equal(tap0))
+			g.Expect(c.PortUUID).Should(Equal(p.uuid))
+
+			p, err = getPortInfo(tap1)
+			g.Expect(err).ShouldNot(HaveOccurred())
+			g.Expect(p).ShouldNot(BeNil())
+			g.Expect(p.brName).Should(Equal(testBr1 + "-policy"))
+			g.Expect(p.intfExternalIDs).Should(Equal(externalIDs1))
+			g.Expect(p.intfIfaceID).Should(Equal(ifaceID1))
+			c, err = getNicConfig(testBr1, types.NicOut)
+			g.Expect(err).ShouldNot(HaveOccurred())
+			g.Expect(c).ShouldNot(BeNil())
+			g.Expect(c.IfaceID).Should(Equal(ifaceID1))
+			g.Expect(c.PortName).Should(Equal(tap1))
+			g.Expect(c.PortUUID).Should(Equal(p.uuid))
+
+			By("clear success")
+			err = Reset(nil)
+			g.Expect(err).ShouldNot(HaveOccurred())
+
+			p, err = getPortInfo(tap0)
+			g.Expect(err).ShouldNot(HaveOccurred())
+			g.Expect(p).ShouldNot(BeNil())
+			g.Expect(p.brName).Should(Equal(svcChainBr))
+			g.Expect(p.intfExternalIDs).Should(Equal(externalIDs0))
+			g.Expect(p.intfIfaceID).Should(Equal(ifaceID0))
+			c, err = getNicConfig(testBr1, types.NicIn)
+			g.Expect(err).ShouldNot(HaveOccurred())
+			g.Expect(c).Should(BeNil())
+
+			p, err = getPortInfo(tap1)
+			g.Expect(err).ShouldNot(HaveOccurred())
+			g.Expect(p).ShouldNot(BeNil())
+			g.Expect(p.brName).Should(Equal(svcChainBr))
+			g.Expect(p.intfExternalIDs).Should(Equal(externalIDs1))
+			g.Expect(p.intfIfaceID).Should(Equal(ifaceID1))
+			c, err = getNicConfig(testBr1, types.NicOut)
+			g.Expect(err).ShouldNot(HaveOccurred())
+			g.Expect(c).Should(BeNil())
+		})
+	})
+})
