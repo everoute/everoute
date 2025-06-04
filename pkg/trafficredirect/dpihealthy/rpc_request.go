98add16c5b658a618b946ad53ae74e5ca7edd32f
Jiao Zhou <jiao.zhou@smartx.com>
2025-06-04 17:33:58 +0800

    feat: refactor flowIDallocator and add trafficredirect metrics and modify dpi healthy check param format

--- pkg/trafficredirect/dpihealthy/rpc_request.go
+++ pkg/trafficredirect/dpihealthy/rpc_request.go
@@ -11,19 +11,27 @@ import (
 )
 
 const (
-	SocketPath    = "/var/run/vs-controller/vs-controller.sock"
-	RPCMethodName = "channel-state"
-	DPIModuleName = "DPI"
+	SocketPath        = "/var/run/vs-controller/vs-controller.sock"
+	RPCMethodName     = "channel-state"
+	DPIModuleName     = "DPI"
+	EverouteRequestID = 2
 )
 
 type Request struct {
-	Method string `json:"method"`
+	JSONRPC string `json:"jsonrpc"`
+	Method  string `json:"method"`
+	// request id
+	ID int `json:"id"`
 }
 
-type Response struct {
+type Result struct {
 	Ec    string            `json:"ec"`
 	Error string            `json:"error"`
 	Data  map[string]string `json:"data"`
+}
+
+type Response struct {
+	Result Result `json:"result"`
 }
 
 func HealthyCheck() types.DPIStatus {
@@ -34,7 +42,11 @@ func HealthyCheck() types.DPIStatus {
 	}
 	defer conn.Close()
 
-	req := Request{Method: RPCMethodName}
+	req := Request{
+		JSONRPC: "2.0",
+		Method:  RPCMethodName,
+		ID:      EverouteRequestID,
+	}
 	reqBytes, err := json.Marshal(req)
 	if err != nil {
 		klog.Errorf("Failed to encode request %v to json: %s", req, err)
@@ -61,18 +73,18 @@ func HealthyCheck() types.DPIStatus {
 		return types.DPIUnknown
 	}
 
-	if resp.Ec != "EOK" || resp.Error != "" {
-		klog.Errorf("Request execute failed, ec: %s, err: %s", resp.Ec, resp.Error)
+	if resp.Result.Ec != "E_OK" || resp.Result.Error != "" {
+		klog.Errorf("Request execute failed, ec: %s, err: %s", resp.Result.Ec, resp.Result.Error)
 		return types.DPIUnknown
 	}
 
-	for k, v := range resp.Data {
+	for k, v := range resp.Result.Data {
 		if k == DPIModuleName {
 			if v == string(types.DPIAlive) || v == string(types.DPIDead) {
 				klog.V(4).Infof("DPI healthy check response status is %s", v)
 				return types.DPIStatus(v)
 			}
-			klog.Infof("DPI healthy check response status is %s", v)
+			klog.Infof("DPI healthy check response status is unknown status %s", v)
 			return types.DPIUnknown
 		}
 	}
